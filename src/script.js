bc969a5179e66b0e38c1f102d22240bb44761c31
Gustavo Caris <gustavocaris14052002@gmail.com>
2026-01-28 22:38:58 -0300

    feat: initialize DOM elements for search and playlist functionality

---
+++ src/script.js
@@ -0,0 +1,5 @@
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlists');
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlists');
